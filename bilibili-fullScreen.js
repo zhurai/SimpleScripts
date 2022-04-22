b64fa095dcf97d4b2d2eb24d48c5a4f7503aa043
zhurai <zhurai.tsuki@gmail.com>
2022-04-22 16:32:52 -0700

    Update bilibili-fullScreen.js
    version 1.1

--- bilibili-fullScreen.js
+++ bilibili-fullScreen.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name	BiliBili Full Screen
-// @version	1
+// @version	1.1
 // @grant	none
 // @match 	https://*.bilibili.com/video/*
 // @require	http://code.jquery.com/jquery-latest.js
@@ -10,10 +10,11 @@
 // This script automatically clicks on the web fullscreen button when you open a bilibili page
 //      This is not a regular windows fullscreen that you would see in youtube full screen, this button just makes 
 // 
-// (old version with old classnames for completeness)
+// Version 1.0: Initial Script
+// Version 1.1: Fixed the Classnames used as the bilibili player changed classnames for everything
 
-waitForKeyElements (".bilibili-player-iconfont-web-fullscreen-off", clickOnFullscreen);
+waitForKeyElements (".bpx-player-ctrl-web-enter", clickOnFullscreen);
 
 function clickOnFullscreen (jNode) {
-	document.getElementsByClassName("bilibili-player-video-web-fullscreen")[0].click();
+	document.getElementsByClassName("bpx-player-ctrl-web")[0].click();
 }
