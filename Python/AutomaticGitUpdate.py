a4c27b9cb2ce5a5ba4909fc4c68f742ca48244e0
zhurai <zhurai.tsuki@gmail.com>
2022-08-10 14:46:17 -0700

    py: autogitupdate: fix script auto push

--- Python/AutomaticGitUpdate.py
+++ Python/AutomaticGitUpdate.py
@@ -1,17 +1,25 @@
+# Generate an SSH-Key for the server
+# Upload the SSH-Key .Pub file to Github (for the repository that the script should automatically auto commit to)
+# Create a script "gitpush.ps1" in the same directory as this file that just contains "git push"
+
 import subprocess
 import os
+import os.path
+import sys
 from datetime import datetime
 
 dateformat="%Y-%m-%d"
 today=datetime.today().strftime(dateformat)
 
-gitexe="C:\\Program Files\\Git\\cmd\\git.exe"
+os.environ["HOME"]=os.path.expanduser('~')
+shell=r"powershell.exe"
+gitexe=r"C:\Program Files\Git\cmd\git.exe"
 gitmsg="automated git updates for " + today
 gitadd="add ."
 gitcommit="commit -m \""+gitmsg+"\""
-gitpush="push"
+gitpush="gitpush.ps1"
 cwd=os.path.dirname(os.path.realpath(__file__))
 
-subprocess.call(gitexe+" "+gitadd,cwd=cwd)
-subprocess.call(gitexe+" "+gitcommit,cwd=cwd)
-subprocess.call(gitexe+" "+gitpush,cwd=cwd)
+subprocess.call(gitexe+" "+gitadd, cwd=cwd)
+subprocess.call(gitexe+" "+gitcommit, cwd=cwd)
+process=subprocess.call('powershell.exe -ExecutionPolicy RemoteSigned -file "' + gitpush +'"', cwd=cwd)
