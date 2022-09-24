c5d864440aa4f7885102225addd03c2a0e2d5e80
zhurai <zhurai.tsuki@gmail.com>
2022-09-24 16:11:21 -0700

    bluetooth: fix default sound device

--- BAT/Bluetooth/Bluetooth-Connect.bat
+++ BAT/Bluetooth/Bluetooth-Connect.bat
@@ -2,3 +2,4 @@ btcom -b AC:12:2F:C8:85:76 -c -s111e
 btcom -b AC:12:2F:C8:85:76 -c -s110b
 TIMEOUT /T 5
 "C:\Program Files (x86)\VB\Voicemeeter\voicemeeter.exe" -R
+"C:\Personal\Programs\nircmd\nircmd.exe" setdefaultsounddevice "VoiceMeeter Input" all
