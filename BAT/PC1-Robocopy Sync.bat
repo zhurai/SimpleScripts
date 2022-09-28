a247eb9f788ba782b67c0983222694d51c99cf7e
zhurai <zhurai.tsuki@gmail.com>
2022-09-27 22:23:17 -0700

    update robocopy sync script, rearrange dir

---
+++ BAT/PC1-Robocopy Sync.bat
@@ -0,0 +1,18 @@
+:: Sync StartupScripts (Autoit) to PersonalSync (Syncthing)
+ROBOCOPY "D:\GitHub\personal\misc-scripts\AutoIt\StartupScripts" "D:\PersonalSync\Scripts\Autoit" /MIR /R:x /W:x
+:: Sync StartupScripts (AHK) to PersonalSync (Syncthing)
+ROBOCOPY "D:\GitHub\personal\misc-scripts\AutoHotKey\StartupScripts" "D:\PersonalSync\Scripts\AutoHotKey" /MIR /R:x /W:x
+:: Sync Robocopy Script (BAT) to PersonalSync (Syncthing)
+
+::ROBOCOPY "D:\GitHub\personal\misc-scripts\BAT\Robocopy" "D:\PersonalSync\Scripts\Sync" /MIR /R:x /W:x
+:: Sync Livestream Assets to PersonalSync (Syncthing)
+::ROBOCOPY "D:\GitHub\personal\livestream" "D:\PersonalSync\Livestream" /MIR /R:x /W:x
+:::: Sync KP to GoogleDrive
+::ROBOCOPY "D:\PersonalSync\Applications\KP" "D:\GoogleDrive\Personal Sync\KP" /MIR /R:x /W:x
+:::: Sync Scripts to GoogleDrive
+::ROBOCOPY "D:\PersonalSync\Scripts" "D:\GoogleDrive\Personal Sync\Scripts" /MIR /R:x /W:x
+:::: Sync Bluetooth Scripts
+::ROBOCOPY "D:\GitHub\personal\misc-scripts\BAT\Bluetooth" "D:\PersonalSync\Scripts\Bluetooth" /MIR /R:x /W:x
+:::: Sync Task Warrior Scripts
+::ROBOCOPY "D:\GitHub\personal\taskwarrior-scripts" "D:\PersonalSync\Applications\TaskWarrior\scripts" /MIR /R:x /W:x /XD .git /XD commit-scripts /XF .gitignore /XF .gitattributes /XD .old
+
--- BAT/Robocopy/PC1-Robocopy Sync.bat
+++ BAT/Robocopy/PC1-Robocopy Sync.bat
@@ -1,20 +1,16 @@
-:: Sync Autoit Startup Automation Scripts to PersonalSync (Syncthing)
-ROBOCOPY "D:\GitHub\personal\StartupScripts\AutoIt\all" "D:\PersonalSync\Scripts\Autoit\all" /MIR /R:x /W:x
-ROBOCOPY "D:\GitHub\personal\StartupScripts\AutoIt\pc1" "D:\PersonalSync\Scripts\Autoit\pc1" /MIR /R:x /W:x
-ROBOCOPY "D:\GitHub\personal\StartupScripts\AutoIt\pc2" "D:\PersonalSync\Scripts\Autoit\pc2" /MIR /R:x /W:x
-ROBOCOPY "D:\GitHub\personal\StartupScripts\AutoIt\pc3" "D:\PersonalSync\Scripts\Autoit\pc3" /MIR /R:x /W:x
-:: Sync AutoHotkey Scripts to PersonalSync (Syncthing)
-ROBOCOPY "D:\GitHub\personal\StartupScripts\AutoHotkeyScripts" "D:\PersonalSync\Scripts\AutoHotKey" /MIR /R:x /W:x
-:: Sync Robocopy Scripts to PersonalSync (Syncthing)
-ROBOCOPY "D:\GitHub\personal\misc-scripts\BAT\Robocopy" "D:\PersonalSync\Scripts\Sync" /MIR /R:x /W:x
-:: Sync Livestream Assets to PersonalSync (Syncthing)
+:: Sync StartupScripts (Autoit) to PersonalSync (Syncthing)
+ROBOCOPY "D:\GitHub\personal\misc-scripts\AutoIt\StartupScripts" "D:\PersonalSync\Scripts\Autoit" /MIR /R:x /W:x
+:: Sync StartupScripts (AHK) to PersonalSync (Syncthing)
+ROBOCOPY "D:\GitHub\personal\misc-scripts\AutoHotKey\StartupScripts" "D:\PersonalSync\Scripts\AutoHotKey" /MIR /R:x /W:x
+:: Sync Robocopy Script (BAT) to PersonalSync (Syncthing)
+ROBOCOPY "D:\GitHub\personal\misc-scripts\BAT" "D:\PersonalSync\Scripts\Sync" "PC1-Robocopy Sync.bat"  /MIR /R:x /W:x
+:: Sync Bluetooth Scripts (BAT) to PersonalSync (Syncthing)
+ROBOCOPY "D:\GitHub\personal\misc-scripts\BAT\Bluetooth" "D:\PersonalSync\Scripts\Bluetooth" /MIR /R:x /W:x
+:: Sync Livestream Assets (Livestream Git) to PersonalSync (Syncthing)
 ROBOCOPY "D:\GitHub\personal\livestream" "D:\PersonalSync\Livestream" /MIR /R:x /W:x
-:: Sync KP to GoogleDrive
+:: Sync Task Warrior Scripts (Taskwarrior-Scripts) to PersonalSync (Syncthing)
+ROBOCOPY "D:\GitHub\personal\taskwarrior-scripts" "D:\PersonalSync\Applications\TaskWarrior\scripts" /MIR /R:x /W:x /XD .git /XD commit-scripts /XF .gitignore /XF .gitattributes /XD .old
+:: Sync KP (PersonalSync) to Google Drive
 ROBOCOPY "D:\PersonalSync\Applications\KP" "D:\GoogleDrive\Personal Sync\KP" /MIR /R:x /W:x
 :: Sync Scripts to GoogleDrive
 ROBOCOPY "D:\PersonalSync\Scripts" "D:\GoogleDrive\Personal Sync\Scripts" /MIR /R:x /W:x
-:: Sync Bluetooth Scripts
-ROBOCOPY "D:\GitHub\personal\misc-scripts\BAT\Bluetooth" "D:\PersonalSync\Scripts\Bluetooth" /MIR /R:x /W:x
-:: Sync Task Warrior Scripts
-ROBOCOPY "D:\GitHub\personal\taskwarrior-scripts" "D:\PersonalSync\Applications\TaskWarrior\scripts" /MIR /R:x /W:x /XD .git /XD commit-scripts /XF .gitignore /XF .gitattributes /XD .old
-
