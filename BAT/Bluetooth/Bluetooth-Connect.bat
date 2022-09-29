33dcb4f424e3b12ef1dfbbcffd6490f4781eb6d1
zhurai <zhurai.tsuki@gmail.com>
2022-09-28 19:07:25 -0700

    change bluetooth script timeout to 10s

--- BAT/Bluetooth/Bluetooth-Connect.bat
+++ BAT/Bluetooth/Bluetooth-Connect.bat
@@ -1,5 +1,5 @@
 btcom -b AC:12:2F:C8:85:76 -c -s111e
 btcom -b AC:12:2F:C8:85:76 -c -s110b
-TIMEOUT /T 5
+TIMEOUT /T 10
 "C:\Program Files (x86)\VB\Voicemeeter\voicemeeter.exe" -R
 "C:\Personal\Programs\nircmd\nircmd.exe" setdefaultsounddevice "VoiceMeeter Input" all
