5fbcd59892b60945ed926b29a809db6d588d7563
zhurai <zhurai.tsuki@gmail.com>
2022-10-01 12:43:53 -0700

    bluetooth for pc2 seems to need 15 seconds

--- BAT/Bluetooth/Bluetooth-Connect.bat
+++ BAT/Bluetooth/Bluetooth-Connect.bat
@@ -1,5 +1,5 @@
 btcom -b AC:12:2F:C8:85:76 -c -s111e
 btcom -b AC:12:2F:C8:85:76 -c -s110b
-TIMEOUT /T 10
+TIMEOUT /T 15
 "C:\Program Files (x86)\VB\Voicemeeter\voicemeeter.exe" -R
 "C:\Personal\Programs\nircmd\nircmd.exe" setdefaultsounddevice "VoiceMeeter Input" all
